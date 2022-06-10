e59dfd4ee3d7b0b73174e7e337634b7893a10255
rsingh234 <106961318+rsingh234@users.noreply.github.com>
2022-06-10 10:53:02 +0530

    Controller
    Controller File

---
+++ Controller/StoreManagerController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Music_Store.Models;
+using System.Data;
+using System.Data.Entity;
+
+namespace Music_Store.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        MusicStoreContext context = new Models.MusicStoreContext();
+        // GET: StoreManager
+        public ActionResult Index()
+        {
+            
+
+            return View(context.AllAlbums.Include("Artist").Include("Genre").ToList());
+        }
+        public ActionResult CreateNewAlbum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewAlbum(Music_Store.Models.Album a)
+        {
+            context.AllAlbums.Add(a);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditAlbum(string title)
+        {
+            Music_Store.Models.MusicStoreContext context = new
+            Models.MusicStoreContext();
+            var albumtoedit =
+            context.AllAlbums.Include("Artist").
+            Include("Genre").Where(a => a.Title == title).SingleOrDefault();
+            return View(albumtoedit);
+        }
+
+        [HttpPost]
+        public ActionResult EditAlbum(Music_Store.Models.Album a)
+        {
+            Music_Store.Models.MusicStoreContext context = new
+            Models.MusicStoreContext();
+            context.Entry(a).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAlbum(string title)
+        {
+            Music_Store.Models.MusicStoreContext context = new
+            Models.MusicStoreContext();
+
+            var albumtodelete = context.AllAlbums.Where(a => a.Title
+                == title).SingleOrDefault();
+            context.AllAlbums.Remove(albumtodelete);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        // GET: StoreManager/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StoreManager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StoreManager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StoreManager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StoreManager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controller/StroreController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Music_Store.Models;
+
+namespace Music_Store.Controllers
+{
+    public class StroreController : Controller
+    {
+        MusicStoreContext context = new MusicStoreContext();
+        // GET: Strore
+        public ActionResult Index()
+        {
+            var genres = context.AllGenres.ToList();
+            ViewBag.GeneresList = new SelectList(genres, "GenreId", "GenreName");
+            return View(genres);
+
+            
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            //var genreModel = new Genre { GenreName = genre };
+            //return View(genreModel);
+            var genreModel = context.AllGenres.Include("Albums").Where(g =>
+                g.GenreName == genre).Single();
+          
+            return View(genreModel);
+
+        }
+
+        [HttpPost]
+        public ActionResult Browse(int genre)
+        {
+            //var genreModel = new Genre { GenreName = genre };
+            //return View(genreModel);
+            var genreModel = context.AllGenres.Include("Albums").Where(g =>
+                g.GenreId == genre).Single();
+
+            return View(genreModel);
+
+        }
+
+        //public string Details(int id)
+        //{
+        //    string message = "Store.Details(), ID = " + id;
+        //    return message;
+        //}
+        public ActionResult Details(string id)
+        {
+            //var album = (from s in context.AllAlbums
+            //             where s.GenreId == id
+            //             //s.Contains("Tutorials")
+            //             select s).FirstOrDefault()  ;
+            var album = context.AllAlbums.Find(id);
+                                   
+
+            return View(album);
+
+            //var album = new Album { Title = "Album " + id };
+            //return View(album);
+        }
+
+
+
+    }
+}
